b6d41b8d489f003eda8852335a44e8f836b27b44
Joe Ferguson <joe@joeferguson.me>
2018-09-29 10:34:59 -0500

    🚧 custom mail templates

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('DEFAULT_MAIL_FROM', 'hello@example.com'),
+        'name' => env('DEFAULT_MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/checkins/{user_id}', 'CheckinController@getCheckinsForUser')->name('checkins.for.user');
